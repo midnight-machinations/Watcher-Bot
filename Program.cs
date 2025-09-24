748de77b4d65ae97c4ff061d7ec97b83223898e0
Thomas <tberrios9800@gmail.com>
2025-09-24 06:18:16 +0000

    Added base bot connection code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using NetCord.Hosting.Gateway;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+// Load bot token from env into appsettings config
+builder.Configuration.AddInMemoryCollection(
+    new Dictionary<string, string?> { ["Discord:Token"] = builder.Configuration["DISCORD_TOKEN"] }
+);
+
+builder.Services.AddDiscordGateway();
+
+var host = builder.Build();
+
+await host.RunAsync();
